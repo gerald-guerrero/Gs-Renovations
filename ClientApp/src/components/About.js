98e551990d8e72423f936ea41d78e3e712958568
Gerald Guerrero <sbgeraldg@gmail.com>
2023-04-19 20:17:06 -0400

    added static content to Home, About, and Services pages

--- ClientApp/src/components/About.js
+++ ClientApp/src/components/About.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 export class About extends Component {
-  static displayName = About.name;
+    static displayName = About.name;
 
-  render() {
-    return (
-      <div>
+    render() {
+        return (
+            <div>
+                <h1>About G's Renovations</h1>
+                <p>G's Renovations is a family-owned and operated home renovation business based in Atlanta, GA. We specialize in helping homeowners transform their living spaces into the homes of their dreams, with a focus on quality craftsmanship and attention to detail.</p>
 
-      </div>
-    );
-  }
+                <h2>Our Story</h2>
+                <p>Founded in 2017 by Gerardo Guerrero, G's Renovations has been serving the Atlanta area for over 5 years. Starting with just a small team of skilled craftsmen, we have grown into a full-service home renovation company with a reputation for excellence.</p>
+                <p>Over the years, we have worked on projects of all sizes and scopes, from simple repairs and upgrades to complete home remodels. Our goal has always been to provide our clients with the highest level of service and workmanship, and to ensure that their renovation projects are completed on time and on budget.</p>
+
+                <h2>Our Team</h2>
+                <p>At G's Renovations, we are proud to have a team of skilled and dedicated professionals who are committed to helping our clients achieve their home renovation goals. From our experienced project managers to our talented craftsmen, each member of our team is passionate about what they do and dedicated to providing the best possible service to our clients.</p>
+                <p>Our team is led by our very own founder, who brings years of experience and expertise to every project we undertake. They are committed to maintaining the high standards of quality and excellence that have become synonymous with the G's Renovations name.</p>
+
+                <h2>Our Promise</h2>
+                <p>At G's Renovations, we are committed to providing our clients with the highest level of service and workmanship. We promise to work closely with you throughout the entire renovation process, from the initial consultation to the final walkthrough, to ensure that your project meets your exact specifications and exceeds your expectations.</p>
+                <p>With G's Renovations, you can trust that your home is in good hands. Contact us today to schedule a consultation and start your renovation journey.</p>
+            </div>
+        );
+    }
 }
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Welcome to G's Renovations</h1>
+                <p>We are a family-owned and operated home renovation business based in Atlanta, GA. With years of experience and a commitment to quality craftsmanship, we can help you transform your house into the home of your dreams.</p>
+                
+                <p>At G's Renovations, we pride ourselves on our attention to detail and our ability to work closely with our clients to bring their visions to life. Whether you have a specific design in mind or need help coming up with ideas, we are here to help every step of the way.</p>
+
+                <h2>Gallery</h2>
+                <p>Take a look at some of our previous projects, and don't hesitate to contact us to schedule a consultation today.</p>
+                <div className="gallery">
+                    <img src="https://via.placeholder.com/300x200" alt="Project 1" />
+                    <img src="https://via.placeholder.com/300x200" alt="Project 2" />
+                    <img src="https://via.placeholder.com/300x200" alt="Project 3" />
+                    <img src="https://via.placeholder.com/300x200" alt="Project 4" />
+                    <img src="https://via.placeholder.com/300x200" alt="Project 5" />
+                    <img src="https://via.placeholder.com/300x200" alt="Project 6" />
+                </div>
+                
+            </div>
+        );
+    }
 }
