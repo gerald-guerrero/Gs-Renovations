b50ac4baf17aadbd2022ad15f1edfa5f26a8a757
Gerald Guerrero <sbgeraldg@gmail.com>
2023-04-20 16:18:50 -0400

    made contact form functional

--- Controllers/SupportController.cs
+++ Controllers/SupportController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Gs_Renovations.Controllers
 {
@@ -6,23 +12,56 @@ namespace Gs_Renovations.Controllers
     [Route("[controller]")]
     public class SupportController : ControllerBase
     {
-        
+        private readonly IConfiguration _configuration;
+
+        public SupportController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost]
         public async Task<IActionResult> SendMessage([FromBody] Dictionary<string, string> data)
         {
-            string name = data["name"];
-            string phone = data["phone"];
-            string email = data["email"];
-            string details = data["details"];
-            
-            Console.WriteLine(name);
-            Console.WriteLine(email);
-            Console.WriteLine(phone);
-            Console.WriteLine(details);
-            
+            try
+            {
+                string name = data["name"];
+                string phone = data["phone"];
+                string email = data["email"];
+                string details = data["details"];
 
-            return Ok();
+                // Get SMTP configuration from Azure configuration
+                string smtpHost = _configuration["Smtp:Host"];
+                int smtpPort = int.Parse(_configuration["Smtp:Port"]);
+                string smtpUsername = _configuration["Smtp:Username"];
+                string smtpPassword = _configuration["Smtp:Password"];
+                string smtpFromEmail = _configuration["Smtp:FromEmail"];
+                string smtpToEmail = _configuration["Smtp:ToEmail"];
+
+                // Create and configure SMTP client
+                using (var smtpClient = new SmtpClient(smtpHost, smtpPort))
+                {
+                    smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                    smtpClient.EnableSsl = true;
+
+                    // Create message
+                    var message = new MailMessage();
+                    message.From = new MailAddress(smtpFromEmail);
+                    message.To.Add(smtpToEmail);
+                    message.Subject = $"Support request from {name}";
+                    message.Body = $"name: {name}\nphone: {phone}\nemail: {email}\ndetails: {details}";
+
+                    // Send message
+                    await smtpClient.SendMailAsync(message);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and return an error response
+                Console.Error.WriteLine(ex.ToString());
+                return StatusCode(500);
+            }
         }
-       
     }
 }
